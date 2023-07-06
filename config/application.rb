3585a63bc6ec6c510de23b343a53e16f949f714e
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-07-06 14:38:03 -0700

    fix github auth

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,14 @@ Bundler.require(*Rails.groups)
 
 module Gitdevplanner
   class Application < Rails::Application
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      if File.exist?(env_file)
+        YAML.safe_load(File.open(env_file)).each do |key, value|
+          ENV[key.to_s] = value
+        end
+      end
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  # get 'home/index'
+  root 'home#index'
+  get 'auth/github/callback', to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
