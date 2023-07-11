285ef69c22280d73f179ba66046da46ea9fe01c0
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-07-10 20:44:23 -0700

    added private repos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,12 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
+
   def index
     nickname = session[:nickname]
     github_access_token = session[:github_access_token]
 
-    user_data = GithubApiService.fetch_user(nickname, github_access_token)
-    print(user_data)
-    @repo_data = GithubApiService.fetch_repos(user_data['repos_url'], github_access_token)
+    # data = GithubApiService.fetch_user_and_repos(nickname, github_access_token)
+    # @repos_data = data[:repos_data]
+    @repos_data = GithubApiService.fetch_repos(github_access_token)
   end
 end
--- app/services/github_api_service.rb
+++ app/services/github_api_service.rb
@@ -1,40 +1,53 @@
 class GithubApiService
-    def self.fetch_user(username, access_token)
-      uri = URI("https://api.github.com/users/#{username}")
-      headers = {
-        'Authorization' => "Bearer #{access_token}",
-        'User-Agent' => 'Your-App-Name'
-      }
+    # def self.fetch_user(username, access_token)
+    #   uri = URI("https://api.github.com/users/#{username}")
+    #   headers = {
+    #     'X-GitHub-Api-Version'=> '2022-11-28',
+    #     'Authorization' => "Bearer #{access_token}",
+    #     'User-Agent' => "GitDevPlanner #{username}"
+    #   }
   
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
+    #   http = Net::HTTP.new(uri.host, uri.port)
+    #   http.use_ssl = true
   
-      request = Net::HTTP::Get.new(uri.path, headers)
+    #   request = Net::HTTP::Get.new(uri.path, headers)
   
-      response = http.request(request)
-      return nil unless response.is_a?(Net::HTTPSuccess)
+    #   response = http.request(request)
+    #   return nil unless response.is_a?(Net::HTTPSuccess)
   
-      user_data = JSON.parse(response.body)
-      user_data
-    end
+    #   user_data = JSON.parse(response.body)
+    #   user_data
+    # end
   
-    def self.fetch_repos(repos_url, access_token)
-      headers = {
-        'Authorization' => "Bearer #{access_token}",
-        'User-Agent' => 'Your-App-Name'
-      }
+    def self.fetch_repos(access_token)
+        uri = URI('https://api.github.com/user/repos')
+        headers = {
+          'Accept' => 'application/vnd.github+json',
+          'Authorization' => "Bearer #{access_token}",
+          'User-Agent' => 'GitDevPlanner'
+        }
     
-      uri = URI(repos_url)
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
     
-      request = Net::HTTP::Get.new(uri.path, headers)
+        request = Net::HTTP::Get.new(uri.path, headers)
     
-      response = http.request(request)
-      return nil unless response.is_a?(Net::HTTPSuccess)
+        response = http.request(request)
+        return nil unless response.is_a?(Net::HTTPSuccess)
     
-      repos_data = JSON.parse(response.body)
-      repos_data
-    end
+        repos_data = JSON.parse(response.body)
+        repos_data
+      end
+  
+    # def self.fetch_user_and_repos(username, access_token)
+    #   user_data = fetch_user(username, access_token)
+  
+    #   return nil if user_data.nil?
+  
+    #   repos_url = user_data['repos_url']
+    #   repos_data = fetch_repos(repos_url, access_token, username)
+  
+    #   { user_data: user_data, repos_data: repos_data }
+    # end
   end
   
