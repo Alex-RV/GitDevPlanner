ee2a9f49b0193bd617987449b842c4d5835b7fa5
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-07-11 17:04:23 -0700

    fixed and added retrieving all data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,11 +8,14 @@ class HomeController < ApplicationController
     # data = GithubApiService.fetch_user_and_repos(nickname, github_access_token)
     # @repos_data = data[:repos_data]
     
-    # @repos_data = GithubApiService.fetch_repos(github_access_token)
-
-    profiles_data = GithubApiService.fetch_collaborators_and_owners(github_access_token)
-    @collaborators_data = profiles_data[:collaborators]
-    @owners_data = profiles_data[:owners]
+    data = GithubApiService.fetch_repos(github_access_token)
+    @repos_data = data[:all_repos]
+    @owners_data = data[:owners]
+    @collaborators_data = GithubApiService.fetch_collaborators(github_access_token, nickname)
+    
+    # profiles_data = GithubApiService.fetch_collaborators_and_owners(github_access_token)
+    # @collaborators_data = profiles_data[:collaborators]
+    # @owners_data = profiles_data[:owners]
     # print(@repos_data)
   end
 end
--- app/services/github_api_service.rb
+++ app/services/github_api_service.rb
@@ -1,47 +1,91 @@
 require 'concurrent-ruby'
 
 class GithubApiService
-  def self.fetch_collaborators_and_owners(access_token)
-    uri = URI('https://api.github.com/user/repos')
-    headers = {
-      'Accept' => 'application/vnd.github+json',
-      'Authorization' => "Bearer #{access_token}",
-      'User-Agent' => 'GitDevPlanner'
-    }
+    def self.fetch_repos(access_token)
+        uri = URI('https://api.github.com/user/repos')
+        headers = {
+          'Accept' => 'application/vnd.github+json',
+          'Authorization' => "Bearer #{access_token}",
+          'User-Agent' => 'GitDevPlanner'
+        }
+    
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+    
+        request = Net::HTTP::Get.new(uri.path, headers)
+    
+        response = http.request(request)
+        return nil unless response.is_a?(Net::HTTPSuccess)
+    
+        user_repos = JSON.parse(response.body)
+    
+        params = {
+          'affiliation' => 'collaborator,contributor',
+          'visibility' => 'all'
+        }
+    
+        uri.query = URI.encode_www_form(params)
+    
+        request = Net::HTTP::Get.new(uri, headers)
+    
+        response = http.request(request)
+        return nil unless response.is_a?(Net::HTTPSuccess)
+    
+        other_repos = JSON.parse(response.body)
+    
+        all_repos = (user_repos + other_repos).uniq { |repo| repo['id'] }
+    
+        # Owners of the repos
+        owners = Set.new
+    
+        other_repos.each do |repo|
+          owner_data = repo['owner']
+    
+          owners.add(owner_data)
+        end
+    
+        { all_repos: all_repos, owners: owners.to_a }
+      end
 
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
 
-    request = Net::HTTP::Get.new(uri.path, headers)
+    def self.fetch_collaborators(access_token, nickname)
+        uri = URI('https://api.github.com/user/repos')
+        headers = {
+          'Accept' => 'application/vnd.github+json',
+          'Authorization' => "Bearer #{access_token}",
+          'User-Agent' => 'GitDevPlanner'
+        }
+    
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+    
+        request = Net::HTTP::Get.new(uri.path, headers)
+    
+        response = http.request(request)
+        return nil unless response.is_a?(Net::HTTPSuccess)
+    
+        repos_data = JSON.parse(response.body)
+    
+        collaborators = Set.new
+    
+        # Use parallel processing to fetch collaborators concurrently
+        futures = []
+    
+        repos_data.each do |repo|
+          collaborators_url = repo['collaborators_url'].gsub('{/collaborator}', '')
+    
+          futures << Concurrent::Future.execute { fetch_users(collaborators_url, access_token, nickname, exclude_self: true) }
+        end
+    
+        # Wait for all futures to complete and collect the results
+        futures.each do |future|
+          collaborators.merge(future.value)
+        end
+    
+        collaborators.to_a.uniq { |user| user['login'] }
+      end
 
-    response = http.request(request)
-    return nil unless response.is_a?(Net::HTTPSuccess)
-
-    repos_data = JSON.parse(response.body)
-
-    collaborators = Set.new
-    owners = Set.new
-
-    # Use parallel processing to fetch collaborators and owners concurrently
-    futures = []
-
-    repos_data.each do |repo|
-      owner_data = repo['owner']
-      collaborators_url = repo['collaborators_url'].gsub('{/collaborator}', '')
-
-      futures << Concurrent::Future.execute { fetch_users(collaborators_url, access_token, exclude_self: true) }
-      owners.add(owner_data) unless owner_data['login'] == fetch_current_user_login(access_token)
-    end
-
-    # Wait for all futures to complete and collect the results
-    futures.each do |future|
-      collaborators.merge(future.value)
-    end
-
-    { collaborators: collaborators.to_a, owners: owners.to_a }
-  end
-
-  def self.fetch_users(url, access_token, exclude_self: false)
+  def self.fetch_users(url, access_token, nickname, exclude_self: false)
     headers = {
       'Accept' => 'application/vnd.github+json',
       'Authorization' => "Bearer #{access_token}",
@@ -60,35 +104,8 @@ class GithubApiService
     users_data = JSON.parse(response.body)
 
     # Exclude self from collaborators data if specified
-    users_data.reject! { |user| exclude_self && user['login'] == fetch_current_user_login(access_token) }
+    users_data.reject! { |user| exclude_self && user['login'] == nickname }
 
     users_data
-  end
-
-  def self.fetch_current_user_login(access_token)
-    cached_login = Rails.cache.read("current_user_login_#{access_token}")
-    return cached_login if cached_login.present?
-
-    uri = URI('https://api.github.com/user')
-    headers = {
-      'Accept' => 'application/vnd.github+json',
-      'Authorization' => "Bearer #{access_token}",
-      'User-Agent' => 'GitDevPlanner'
-    }
-
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-
-    request = Net::HTTP::Get.new(uri.request_uri, headers)
-
-    response = http.request(request)
-    return nil unless response.is_a?(Net::HTTPSuccess)
-
-    user_data = JSON.parse(response.body)
-    login = user_data['login']
-
-    Rails.cache.write("current_user_login_#{access_token}", login, expires_in: 1.hour) # Cache the current user login for faster access
-
-    login
   end
 end
