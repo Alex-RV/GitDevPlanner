f7fe9a76b79bcffdc659e378bd6c98ccccafaca3
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-07-11 17:37:00 -0700

    optimized api calls with promises

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,17 +5,12 @@ class HomeController < ApplicationController
     nickname = session[:nickname]
     github_access_token = session[:github_access_token]
 
-    # data = GithubApiService.fetch_user_and_repos(nickname, github_access_token)
-    # @repos_data = data[:repos_data]
-    
-    data = GithubApiService.fetch_repos(github_access_token)
-    @repos_data = data[:all_repos]
-    @owners_data = data[:owners]
-    @collaborators_data = GithubApiService.fetch_collaborators(github_access_token, nickname)
-    
-    # profiles_data = GithubApiService.fetch_collaborators_and_owners(github_access_token)
-    # @collaborators_data = profiles_data[:collaborators]
-    # @owners_data = profiles_data[:owners]
-    # print(@repos_data)
+    repos_future = Concurrent::Promise.execute { GithubApiService.fetch_repos(github_access_token) }
+    collaborators_future = Concurrent::Promise.execute { GithubApiService.fetch_collaborators(github_access_token, nickname) }
+
+    data = Concurrent::Promise.zip(repos_future, collaborators_future).value
+    @repos_data = data[0][:all_repos]
+    @owners_data = data[0][:owners]
+    @collaborators_data = data[1]
   end
 end
--- app/services/github_api_service.rb
+++ app/services/github_api_service.rb
@@ -1,89 +1,88 @@
 require 'concurrent-ruby'
 
 class GithubApiService
-    def self.fetch_repos(access_token)
-        uri = URI('https://api.github.com/user/repos')
-        headers = {
-          'Accept' => 'application/vnd.github+json',
-          'Authorization' => "Bearer #{access_token}",
-          'User-Agent' => 'GitDevPlanner'
-        }
-    
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-    
-        request = Net::HTTP::Get.new(uri.path, headers)
-    
-        response = http.request(request)
-        return nil unless response.is_a?(Net::HTTPSuccess)
-    
-        user_repos = JSON.parse(response.body)
-    
-        params = {
-          'affiliation' => 'collaborator,contributor',
-          'visibility' => 'all'
-        }
-    
-        uri.query = URI.encode_www_form(params)
-    
-        request = Net::HTTP::Get.new(uri, headers)
-    
-        response = http.request(request)
-        return nil unless response.is_a?(Net::HTTPSuccess)
-    
-        other_repos = JSON.parse(response.body)
-    
-        all_repos = (user_repos + other_repos).uniq { |repo| repo['id'] }
-    
-        # Owners of the repos
-        owners = Set.new
-    
-        other_repos.each do |repo|
-          owner_data = repo['owner']
-    
-          owners.add(owner_data)
-        end
-    
-        { all_repos: all_repos, owners: owners.to_a }
-      end
+  def self.fetch_repos(access_token)
+    uri = URI('https://api.github.com/user/repos')
+    headers = {
+      'Accept' => 'application/vnd.github+json',
+      'Authorization' => "Bearer #{access_token}",
+      'User-Agent' => 'GitDevPlanner'
+    }
 
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
 
-    def self.fetch_collaborators(access_token, nickname)
-        uri = URI('https://api.github.com/user/repos')
-        headers = {
-          'Accept' => 'application/vnd.github+json',
-          'Authorization' => "Bearer #{access_token}",
-          'User-Agent' => 'GitDevPlanner'
-        }
-    
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-    
-        request = Net::HTTP::Get.new(uri.path, headers)
-    
-        response = http.request(request)
-        return nil unless response.is_a?(Net::HTTPSuccess)
-    
-        repos_data = JSON.parse(response.body)
-    
-        collaborators = Set.new
-    
-        # Use parallel processing to fetch collaborators concurrently
-        futures = []
-    
-        repos_data.each do |repo|
-          collaborators_url = repo['collaborators_url'].gsub('{/collaborator}', '')
-    
-          futures << Concurrent::Future.execute { fetch_users(collaborators_url, access_token, nickname, exclude_self: true) }
-        end
-    
-        # Wait for all futures to complete and collect the results
-        futures.each do |future|
-          collaborators.merge(future.value)
-        end
-    
-        collaborators.to_a.uniq { |user| user['login'] }
-      end
+    request = Net::HTTP::Get.new(uri.path, headers)
+
+    response = http.request(request)
+    return nil unless response.is_a?(Net::HTTPSuccess)
+
+    user_repos = JSON.parse(response.body)
+
+    params = {
+      'affiliation' => 'collaborator,contributor',
+      'visibility' => 'all'
+    }
+
+    uri.query = URI.encode_www_form(params)
+
+    request = Net::HTTP::Get.new(uri, headers)
+
+    response = http.request(request)
+    return nil unless response.is_a?(Net::HTTPSuccess)
+
+    other_repos = JSON.parse(response.body)
+
+    all_repos = (user_repos + other_repos).uniq { |repo| repo['id'] }
+
+    # Owners of the repos
+    owners = Set.new
+
+    other_repos.each do |repo|
+      owner_data = repo['owner']
+
+      owners.add(owner_data)
+    end
+
+    { all_repos: all_repos, owners: owners.to_a }
+  end
+
+  def self.fetch_collaborators(access_token, nickname)
+    uri = URI('https://api.github.com/user/repos')
+    headers = {
+      'Accept' => 'application/vnd.github+json',
+      'Authorization' => "Bearer #{access_token}",
+      'User-Agent' => 'GitDevPlanner'
+    }
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Get.new(uri.path, headers)
+
+    response = http.request(request)
+    return nil unless response.is_a?(Net::HTTPSuccess)
+
+    repos_data = JSON.parse(response.body)
+
+    collaborators = Concurrent::Set.new
+
+    # Use parallel processing to fetch collaborators concurrently
+    promises = []
+
+    repos_data.each do |repo|
+      collaborators_url = repo['collaborators_url'].gsub('{/collaborator}', '')
+
+      promises << Concurrent::Promise.execute { fetch_users(collaborators_url, access_token, nickname, exclude_self: true) }
+    end
+
+    # Wait for all promises to complete and collect the results
+    promises.each do |promise|
+      promise.value.each { |user| collaborators.add(user) }
+    end
+
+    collaborators.to_a.uniq { |user| user['login'] }
+  end
 
   def self.fetch_users(url, access_token, nickname, exclude_self: false)
     headers = {
