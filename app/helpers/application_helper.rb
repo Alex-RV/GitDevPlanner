b3873707a495a3ebe30530c2c2d46a320f0b5417
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-07-12 20:57:54 -0700

    added tailwind, sidebar, subscriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
+# app/helpers/application_helper.rb
+
 module ApplicationHelper
-end
+    def fetch_data(url, access_token)
+      headers = {
+        'Accept' => 'application/vnd.github+json',
+        'Authorization' => "Bearer #{access_token}",
+        'User-Agent' => 'GitDevPlanner'
+      }
+  
+      uri = URI(url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+  
+      request = Net::HTTP::Get.new(uri.path, headers)
+  
+      response = http.request(request)
+      return [] unless response.is_a?(Net::HTTPSuccess)
+  
+      JSON.parse(response.body)
+    end
+  end
+  
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,33 @@
+# app/helpers/home_helper.rb
+
 module HomeHelper
-end
+    def fetch_subscriptions(collaborator_url, access_token)
+      fetch_data("#{collaborator_url}/subscriptions", access_token)
+    end
+  
+    def fetch_organizations(collaborator_url, access_token)
+      fetch_data("#{collaborator_url}/orgs", access_token)
+    end
+  
+    private
+  
+    def fetch_data(url, access_token)
+      headers = {
+        'Accept' => 'application/vnd.github+json',
+        'Authorization' => "Bearer #{access_token}",
+        'User-Agent' => 'GitDevPlanner'
+      }
+  
+      uri = URI(url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+  
+      request = Net::HTTP::Get.new(uri.path, headers)
+  
+      response = http.request(request)
+      return [] unless response.is_a?(Net::HTTPSuccess)
+  
+      JSON.parse(response.body)
+    end
+  end
+  
