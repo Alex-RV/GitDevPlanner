e7342b6ae938ad4cdb66b85616c91c4a5f5d4eb1
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-07-25 19:14:14 -0700

    add db for people view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -24,16 +24,12 @@ class DashboardController < ApplicationController
   end
 
   def people
+    @user = current_user
+    @collaborators_data = @user.collaborators
+    @owners_data = @user.owners
+
     nickname = session[:nickname]
     github_access_token = session[:github_access_token]
-
-    repos_future = Concurrent::Promise.execute { GithubApiService.fetch_repos(github_access_token, nickname) }
-    collaborators_future = Concurrent::Promise.execute { GithubApiService.fetch_collaborators(github_access_token, nickname) }
-
-    data = Concurrent::Promise.zip(repos_future, collaborators_future).value
-    @repos_data = data[0][:all_repos]
-    @owners_data = data[0][:owners]
-    @collaborators_data = data[1]
     GithubPeopleJob.perform_later(github_access_token, nickname)
   end
 
