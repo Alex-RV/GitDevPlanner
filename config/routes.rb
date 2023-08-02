80b538358f971e504ab554a80af7c19c5a7c64d5
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-08-01 22:56:45 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/repositories', to: 'dashboard#repositories'
   get '/people', to: 'dashboard#people'
   get '/settings', to: 'dashboard#settings', as: 'settings'
-  root 'home#index'
+  root to: redirect('/dashboard') 
   get 'auth/github/callback', to: 'sessions#create'
   get '/login', to: 'sessions#login'
   delete '/logout', to: 'sessions#delete'
