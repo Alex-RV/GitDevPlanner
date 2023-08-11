eecdcc33bbe9f676c97a4a51564a2924010765cc
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-08-10 19:54:24 -0700

    Update data_controller.rb

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,13 +1,5 @@
 class DataController < ApplicationController
-  @@job_triggered = false
 
   def fetch_data
-    if @@job_triggered
-      render json: { status: 'Data fetch job already triggered' }
-    else
-      GithubDataJob.perform_later(session[:github_access_token], session[:nickname])
-      @@job_triggered = true
-      render json: { status: 'Data fetch job enqueued' }
-    end
   end
 end
